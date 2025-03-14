13214f272b001db45a9ca36e8b24a009beea6bca
heewon43 <105628552+heewon43@users.noreply.github.com>
2025-03-14 19:01:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll(".circle").forEach(circle => {
         let percent = circle.getAttribute("data-percent");
-        circle.style.setProperty("--percent", percent);
+        let degree = (percent / 100) * 360; // 퍼센트를 각도로 변환
+        circle.style.background = `conic-gradient(#6FCF97 ${degree}deg, #333 ${degree}deg)`;
     });
 });
